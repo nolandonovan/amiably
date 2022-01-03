995676dbd59babeaf01a271b193800601748b90e
nolandonovan <nolandonovan24@gmail.com>
2022-01-02 18:43:04 -0600

    Initialized Next.js project

---
+++ lib/firebase.ts
@@ -0,0 +1,32 @@
+import firebase from 'firebase/compat/app';
+// commenting out analytics for now
+// import { getAnalytics } from "firebase/analytics";
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+import 'firebase/compat/storage';
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCl5VkuyICvX2HY1yzoD1Kx_1d-5uFLQPI",
+  authDomain: "amiably-io.firebaseapp.com",
+  projectId: "amiably-io",
+  storageBucket: "amiably-io.appspot.com",
+  messagingSenderId: "1019369072078",
+  appId: "1:1019369072078:web:50cf8961f50508cd61379f",
+  measurementId: "G-42HM2HQZXX"
+};
+
+// Initialize Firebase
+if (!firebase.apps.length){
+    firebase.initializeApp(firebaseConfig);
+    // commenting out analytics for now. will need to make the line above a const named 'app'
+    // const analytics = getAnalytics(app);
+}
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+export const storage = firebase.storage();
