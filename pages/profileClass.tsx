d204f87127e055fe891fe496b637f2c640bce5ca
Zoe Richardson <z.richardson.business@gmail.com>
2022-02-16 15:33:32 -0600

    Update profileClass.tsx
    Creating a profile class from a profile function component so that i can use states more dynamically

---
+++ pages/profileClass.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import { useState, Component } from 'react';
+import ReactDOM from 'react-dom';
+import StyleSheet from '../styles/styleSheet.module.css';
+import Edit from './editProfile';
+import ProfileInfo from './profileInfo';
+
+
+// // Profile : BEGIN
+// const contacts: [
+//   {id: 1, "name": "Zoe", "lastName": "Richardson"},
+// {id: 2, "name": "Noly", "lastName": "Donovan"}
+// ];
+
+ 
+class Profile extends React.Component {
+    state = { :  }
+    render() { 
+        return (  
+
+
+
+            
+        );
+    }
+}
+ 
+export default Profile;
