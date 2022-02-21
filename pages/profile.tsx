5bd719be4e57529e442b81e0dfbe297669f5b8c9
crimsonRIBBON <84585332+crimsonRIBBON@users.noreply.github.com>
2022-02-21 08:47:31 -0600

    setting up Detail page as a class rather than Functional Component

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -2,14 +2,14 @@ import * as React from 'react';
 import { useState, Component } from 'react';
 import ReactDOM from 'react-dom';
 import StyleSheet from '../styles/styleSheet.module.css';
-import Edit from './editProfile';
+// import Edit from './editProfile';
 import ProfileInfo from './profileInfo';
 
 
 // Profile : BEGIN
-const contacts: [
-  {id: 1, "name": "Zoe", "lastName": "Richardson"},
-{id: 2, "name": "Noly", "lastName": "Donovan"}
+const contacts = [
+  {id: 1, name: "Zoe", lastName: "Richardson"},
+{id: 2, name: "Noly", lastName: "Donovan"}
 ];
 
 
@@ -124,6 +124,5 @@ return (
 </div>
 
 );
-
 };
  
--- pages/profileClass.tsx
+++ pages/profileClass.tsx
@@ -2,25 +2,135 @@ import * as React from 'react';
 import { useState, Component } from 'react';
 import ReactDOM from 'react-dom';
 import StyleSheet from '../styles/styleSheet.module.css';
-import Edit from './editProfile';
+// import Edit from './editProfile';
 import ProfileInfo from './profileInfo';
 
 
-// // Profile : BEGIN
-// const contacts: [
-//   {id: 1, "name": "Zoe", "lastName": "Richardson"},
-// {id: 2, "name": "Noly", "lastName": "Donovan"}
-// ];
-
+// interface ProfileProps {
+//     contacts: [
+//         {id: 1, name: "Zoe", lastName: "Richardson"},
+//       {id: 2, name: "Noly", lastName: "Donovan"}
+//       ];
+// }
+ 
+// interface ProfileState {
+    
+// }
  
 class Profile extends React.Component {
-    state = { :  }
+
+    // const [isEditing, setIsEditing] = useState(false);
+
+    state = {
+        isEditing: false,
+        contacts: [
+            {id: 1, name: "Zoe", lastName: "Richardson"},
+            {id: 2, name: "Noly", lastName: "Donovan"}
+        ]
+      };
+
+    handleEditing = () => {
+       ( this.state.isEditing === false) ?
+         this.setState({ isEditing: true})  
+        :  this.setState({isEditing: false})
+    }
+
     render() { 
         return (  
+             
+<div className={StyleSheet.profileBackground}>
+
+{ 
+ this.state.isEditing ? (<h1>hi</h1>)
+  : (<h1>bye</h1>)}
+
+<body className={StyleSheet.bodyProfile}>
+   
+
+{/* <!-- Hero Image, Flush : BEGIN --> */}
+
+          <tr className={StyleSheet.hero}>
+           <td>
+               <img src={"/IMAGES/wp3198779.jpg"} className={StyleSheet.heroImage}/>
+           </td>
+          </tr>
+          
+          
+{/* <!-- Hero Image, Flush : END --> */}
+
+{/* <!-- 1 BIO : BEGIN --> */}
+ <tr className={StyleSheet.profileTab}>
+   <td>
+       <table className={StyleSheet.tableProfile}> 
+   
+     {/* <ProfileInfo key={this.state.contacts} name={this.state.contacts} /> */}
+     <div className={"CenterProfile"}>
+     <img src={"/IMAGES/SHbrunet1.png"} className={StyleSheet.profilePic}/>
+     <p className={"friendHeader"}>
+         </p>
+         
+         <span></span>
+     </div>
+ 
+       <tr>
+         
+         <td> 
+           
+         {/* <!-- NOTHING IN THIS DATA CELL --> */}
+           {/* <!-- Button : Begin --> */}
+         
+           <table className={StyleSheet.tableProfile} >
+     
+             <tr>
+               <td className={StyleSheet.tableDataProfile}>
+
+                
+             </td>
+             </tr>
+           </table>
+         {/*<!-- Button : END --> */}
+         
+         </td>
+       </tr>
+     </table></td>
+ </tr>
+        {/* <!-- BIO : END --> */}
+        {/* <!-- Two Even Columns : BEGIN --> */}
+<tr> 
+
+<td className={StyleSheet.buttonsTable}>
+ <table className={StyleSheet.a}>
+   <tr>
+
+     <td className={StyleSheet.b}>
+     
+     <textarea name="profile" cols="70" rows="10"></textarea>
+     <br/>
+
+     <div>
+
+     <button type="submit" className={StyleSheet.buttonCreate} onClick={() => this.handleEditing()}>{}</button> 
+           {/* {
+          
+           <button type="submit" className={StyleSheet.buttonCreate} onClick={() => this.handleEditing()}>{}</button> 
+           <button type="submit" className={StyleSheet.buttonCreate} onClick={() => this.handleEditing()}>{}</button> 
+          } */}
+     </div>
+   </td>
+</tr>
+     
+   </table> 
+ </td>
+
+</tr>
+{/* <!-- Two Even Columns : END --> */}
+
+</body>
 
 
 
-            
+</div>
+
         );
     }
 }
