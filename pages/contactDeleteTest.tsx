b6f3e1d43cf03820802e30d6603462d71e80210a
nolandonovan <nolandonovan24@gmail.com>
2022-02-24 14:13:13 -0600

    delete contact proof of concept

---
+++ pages/contactDeleteTest.tsx
@@ -0,0 +1,30 @@
+import { firestore, auth } from "../lib/firebase";
+import { useRouter } from "next/router"
+import toast from 'react-hot-toast';
+
+export default function Page({ }) {
+    const router = useRouter();
+
+    const id = "NYG4l0ihL40s4hxqnXns"
+    const uid = auth.currentUser.uid;
+    const contactRef = firestore.collection('users').doc(uid).collection('contacts').doc(id);
+
+    async function deleteContact(){
+        await contactRef.delete()
+        .then(res => {
+           console.log("Contact deleted!");
+           router.push('/')
+           toast.success("Contact Deleted")
+        }).catch(error => {
+           console.error("Error deleting contact: ", error);
+        });
+    }
+
+  return (
+      <main>
+          <h1>Testing Delete functionality</h1>
+          <p>Contact ID: {id}</p>
+          <button onClick={deleteContact}>delete</button>
+      </main>
+  )
+}
